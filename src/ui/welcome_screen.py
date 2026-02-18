691a8ddf1e6271901d8dd132565240850bbb8cbc
Alston Chan <git@chanalston.com>
2026-02-18 18:11:18 +0800

    refactor: welcome screen

--- src/ui/welcome_screen.py
+++ src/ui/welcome_screen.py
@@ -42,163 +42,30 @@ class WelcomeFrame(object):
             WelcomeFrame.setObjectName("WelcomeFrame")
         WelcomeFrame.resize(510, 488)
         WelcomeFrame.setAutoFillBackground(False)
-        self.gridLayout_2 = QGridLayout(WelcomeFrame)
-        self.gridLayout_2.setObjectName("gridLayout_2")
+
+        self.gridLayoutParent = QGridLayout(WelcomeFrame)
+        self.gridLayoutParent.setObjectName("gridLayoutParent")
+
         self.gridLayoutMain = QGridLayout()
         self.gridLayoutMain.setObjectName("gridLayoutMain")
-        self.horizontalLayoutLogo = QHBoxLayout()
-        self.horizontalLayoutLogo.setSpacing(0)
-        self.horizontalLayoutLogo.setObjectName("horizontalLayoutLogo")
-        self.horizontalLayoutLogo.setSizeConstraint(
-            QLayout.SizeConstraint.SetDefaultConstraint
-        )
-        self.horizontalSpacerLogoLeft = QSpacerItem(
-            40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
-        )
 
-        self.horizontalLayoutLogo.addItem(self.horizontalSpacerLogoLeft)
-
-        self.label_3 = QLabel(WelcomeFrame)
-        self.label_3.setObjectName("label_3")
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.label_3.sizePolicy().hasHeightForWidth())
-        self.label_3.setSizePolicy(sizePolicy)
-        font = QFont()
-        font.setPointSize(17)
-        self.label_3.setFont(font)
-        self.label_3.setPixmap(QPixmap(":/icons/logo.png"))
-        self.label_3.setScaledContents(True)
-        self.label_3.setAlignment(
-            Qt.AlignmentFlag.AlignBottom | Qt.AlignmentFlag.AlignHCenter
-        )
-
-        self.horizontalLayoutLogo.addWidget(self.label_3)
-
-        self.horizontalSpacerLogoRight = QSpacerItem(
-            40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
-        )
-
-        self.horizontalLayoutLogo.addItem(self.horizontalSpacerLogoRight)
-
-        self.gridLayoutMain.addLayout(self.horizontalLayoutLogo, 1, 0, 1, 1)
-
-        self.horizontalLayoutCTA = QHBoxLayout()
-        self.horizontalLayoutCTA.setObjectName("horizontalLayoutCTA")
-        self.horizontalSpacerCTALeft = QSpacerItem(
-            40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
-        )
-
-        self.horizontalLayoutCTA.addItem(self.horizontalSpacerCTALeft)
-
-        self.pushButtonLoad = QPushButton(WelcomeFrame)
-        self.pushButtonLoad.setObjectName("pushButtonLoad")
-        sizePolicy1 = QSizePolicy(
-            QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum
-        )
-        sizePolicy1.setHorizontalStretch(76)
-        sizePolicy1.setVerticalStretch(30)
-        sizePolicy1.setHeightForWidth(
-            self.pushButtonLoad.sizePolicy().hasHeightForWidth()
-        )
-        self.pushButtonLoad.setSizePolicy(sizePolicy1)
-        self.pushButtonLoad.setMinimumSize(QSize(0, 0))
-        self.pushButtonLoad.setMaximumSize(QSize(100, 30))
-        self.pushButtonLoad.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
-        icon = QIcon(":/icons/email_open.png")
-        icon.addFile(
-            ":/icons/email_open.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off
-        )
-        self.pushButtonLoad.setIcon(icon)
-        self.pushButtonLoad.setFlat(False)
-
-        self.horizontalLayoutCTA.addWidget(self.pushButtonLoad)
-
-        self.horizontalSpacerCTARight = QSpacerItem(
-            40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
-        )
-
-        self.horizontalLayoutCTA.addItem(self.horizontalSpacerCTARight)
-
-        self.gridLayoutMain.addLayout(self.horizontalLayoutCTA, 3, 0, 1, 1)
-
+        # Vertical spacers to push content to centre
         self.verticalSpacerTop = QSpacerItem(
             20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding
         )
-
         self.gridLayoutMain.addItem(self.verticalSpacerTop, 0, 0, 1, 1)
-
-        self.verticalLayoutText = QVBoxLayout()
-        self.verticalLayoutText.setObjectName("verticalLayoutText")
-        self.verticalLayoutText.setContentsMargins(-1, 10, -1, 10)
-        self.horizontalLayoutTitle = QHBoxLayout()
-        self.horizontalLayoutTitle.setObjectName("horizontalLayoutTitle")
-        self.horizontalSpacerTitleLeft = QSpacerItem(
-            40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
-        )
-
-        self.horizontalLayoutTitle.addItem(self.horizontalSpacerTitleLeft)
-
-        self.labelWelcomeTo = QLabel(WelcomeFrame)
-        self.labelWelcomeTo.setObjectName("labelWelcomeTo")
-        sizePolicy2 = QSizePolicy(
-            QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred
-        )
-        sizePolicy2.setHorizontalStretch(0)
-        sizePolicy2.setVerticalStretch(0)
-        sizePolicy2.setHeightForWidth(
-            self.labelWelcomeTo.sizePolicy().hasHeightForWidth()
-        )
-        self.labelWelcomeTo.setSizePolicy(sizePolicy2)
-        self.labelWelcomeTo.setFont(font)
-        self.labelWelcomeTo.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        self.horizontalLayoutTitle.addWidget(self.labelWelcomeTo)
-
-        self.labelAppName = QLabel(WelcomeFrame)
-        self.labelAppName.setObjectName("labelAppName")
-        sizePolicy2.setHeightForWidth(
-            self.labelAppName.sizePolicy().hasHeightForWidth()
-        )
-        self.labelAppName.setSizePolicy(sizePolicy2)
-        font1 = QFont()
-        font1.setFamilies(["STSong"])
-        font1.setPointSize(17)
-        font1.setBold(True)
-        self.labelAppName.setFont(font1)
-        self.labelAppName.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        self.horizontalLayoutTitle.addWidget(self.labelAppName)
-
-        self.horizontalSpacerTitleRight = QSpacerItem(
-            40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
-        )
-
-        self.horizontalLayoutTitle.addItem(self.horizontalSpacerTitleRight)
-
-        self.verticalLayoutText.addLayout(self.horizontalLayoutTitle)
-
-        self.labelDesc = QLabel(WelcomeFrame)
-        self.labelDesc.setObjectName("labelDesc")
-        font2 = QFont()
-        font2.setPointSize(11)
-        self.labelDesc.setFont(font2)
-        self.labelDesc.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        self.verticalLayoutText.addWidget(self.labelDesc)
-
-        self.gridLayoutMain.addLayout(self.verticalLayoutText, 2, 0, 1, 1)
 
         self.verticalSpacerBottom = QSpacerItem(
             20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding
         )
-
         self.gridLayoutMain.addItem(self.verticalSpacerBottom, 4, 0, 1, 1)
 
-        self.gridLayoutMain.setRowMinimumHeight(3, 40)
+        self._setup_logo_section(WelcomeFrame)
+        self._setup_text_section(WelcomeFrame)
+        self._setup_cta_section(WelcomeFrame)
 
-        self.gridLayout_2.addLayout(self.gridLayoutMain, 0, 0, 1, 1)
+        self.gridLayoutMain.setRowMinimumHeight(3, 40)
+        self.gridLayoutParent.addLayout(self.gridLayoutMain, 0, 0, 1, 1)
 
         self.retranslateUi(WelcomeFrame)
 
@@ -208,11 +75,151 @@ class WelcomeFrame(object):
 
     # setupUi
 
+    def _setup_logo_section(self, WelcomeFrame):
+        """Set up the centred logo image at row 1."""
+        self.horizontalLayoutLogo = QHBoxLayout()
+        self.horizontalLayoutLogo.setSpacing(0)
+        self.horizontalLayoutLogo.setObjectName("horizontalLayoutLogo")
+        self.horizontalLayoutLogo.setSizeConstraint(
+            QLayout.SizeConstraint.SetDefaultConstraint
+        )
+
+        self.horizontalSpacerLogoLeft = QSpacerItem(
+            40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
+        )
+        self.horizontalLayoutLogo.addItem(self.horizontalSpacerLogoLeft)
+
+        self.labelLogo = QLabel(WelcomeFrame)
+        self.labelLogo.setObjectName("labelLogo")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.labelLogo.sizePolicy().hasHeightForWidth())
+        self.labelLogo.setSizePolicy(sizePolicy)
+        font = QFont()
+        font.setPointSize(17)
+        self.labelLogo.setFont(font)
+        self.labelLogo.setPixmap(QPixmap(":/icons/logo.png"))
+        self.labelLogo.setScaledContents(True)
+        self.labelLogo.setAlignment(
+            Qt.AlignmentFlag.AlignBottom | Qt.AlignmentFlag.AlignHCenter
+        )
+        self.horizontalLayoutLogo.addWidget(self.labelLogo)
+
+        self.horizontalSpacerLogoRight = QSpacerItem(
+            40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
+        )
+        self.horizontalLayoutLogo.addItem(self.horizontalSpacerLogoRight)
+
+        self.gridLayoutMain.addLayout(self.horizontalLayoutLogo, 1, 0, 1, 1)
+
+    def _setup_text_section(self, WelcomeFrame):
+        """Set up the welcome title and description labels at row 2."""
+        font_title = QFont()
+        font_title.setPointSize(17)
+
+        font_title_bold = QFont()
+        font_title_bold.setFamilies(["STSong"])
+        font_title_bold.setPointSize(17)
+        font_title_bold.setBold(True)
+
+        font_desc = QFont()
+        font_desc.setPointSize(11)
+
+        self.verticalLayoutText = QVBoxLayout()
+        self.verticalLayoutText.setObjectName("verticalLayoutText")
+        self.verticalLayoutText.setContentsMargins(-1, 10, -1, 10)
+
+        # Title row: "Welcome to" + app name
+        self.horizontalLayoutTitle = QHBoxLayout()
+        self.horizontalLayoutTitle.setObjectName("horizontalLayoutTitle")
+
+        self.horizontalSpacerTitleLeft = QSpacerItem(
+            40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
+        )
+        self.horizontalLayoutTitle.addItem(self.horizontalSpacerTitleLeft)
+
+        self.labelWelcomeTo = QLabel(WelcomeFrame)
+        self.labelWelcomeTo.setObjectName("labelWelcomeTo")
+        sizePolicy = QSizePolicy(
+            QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred
+        )
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.labelWelcomeTo.sizePolicy().hasHeightForWidth()
+        )
+        self.labelWelcomeTo.setSizePolicy(sizePolicy)
+        self.labelWelcomeTo.setFont(font_title)
+        self.labelWelcomeTo.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.horizontalLayoutTitle.addWidget(self.labelWelcomeTo)
+
+        self.labelAppName = QLabel(WelcomeFrame)
+        self.labelAppName.setObjectName("labelAppName")
+        sizePolicy.setHeightForWidth(self.labelAppName.sizePolicy().hasHeightForWidth())
+        self.labelAppName.setSizePolicy(sizePolicy)
+        self.labelAppName.setFont(font_title_bold)
+        self.labelAppName.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.horizontalLayoutTitle.addWidget(self.labelAppName)
+
+        self.horizontalSpacerTitleRight = QSpacerItem(
+            40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
+        )
+        self.horizontalLayoutTitle.addItem(self.horizontalSpacerTitleRight)
+
+        self.verticalLayoutText.addLayout(self.horizontalLayoutTitle)
+
+        # Description label
+        self.labelDesc = QLabel(WelcomeFrame)
+        self.labelDesc.setObjectName("labelDesc")
+        self.labelDesc.setFont(font_desc)
+        self.labelDesc.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.verticalLayoutText.addWidget(self.labelDesc)
+
+        self.gridLayoutMain.addLayout(self.verticalLayoutText, 2, 0, 1, 1)
+
+    def _setup_cta_section(self, WelcomeFrame):
+        """Set up the centred 'Load File' button at row 3."""
+        self.horizontalLayoutCTA = QHBoxLayout()
+        self.horizontalLayoutCTA.setObjectName("horizontalLayoutCTA")
+
+        self.horizontalSpacerCTALeft = QSpacerItem(
+            40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
+        )
+        self.horizontalLayoutCTA.addItem(self.horizontalSpacerCTALeft)
+
+        self.pushButtonLoad = QPushButton(WelcomeFrame)
+        self.pushButtonLoad.setObjectName("pushButtonLoad")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
+        sizePolicy.setHorizontalStretch(76)
+        sizePolicy.setVerticalStretch(30)
+        sizePolicy.setHeightForWidth(
+            self.pushButtonLoad.sizePolicy().hasHeightForWidth()
+        )
+        self.pushButtonLoad.setSizePolicy(sizePolicy)
+        self.pushButtonLoad.setMinimumSize(QSize(0, 0))
+        self.pushButtonLoad.setMaximumSize(QSize(100, 30))
+        self.pushButtonLoad.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+        icon = QIcon(":/icons/email_open.png")
+        icon.addFile(
+            ":/icons/email_open.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off
+        )
+        self.pushButtonLoad.setIcon(icon)
+        self.pushButtonLoad.setFlat(False)
+        self.horizontalLayoutCTA.addWidget(self.pushButtonLoad)
+
+        self.horizontalSpacerCTARight = QSpacerItem(
+            40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
+        )
+        self.horizontalLayoutCTA.addItem(self.horizontalSpacerCTARight)
+
+        self.gridLayoutMain.addLayout(self.horizontalLayoutCTA, 3, 0, 1, 1)
+
     def retranslateUi(self, WelcomeFrame):
         WelcomeFrame.setWindowTitle(
             QCoreApplication.translate("WelcomeFrame", "Frame", None)
         )
-        self.label_3.setText("")
+        self.labelLogo.setText("")
         self.pushButtonLoad.setText(
             QCoreApplication.translate("WelcomeFrame", "Load File", None)
         )
